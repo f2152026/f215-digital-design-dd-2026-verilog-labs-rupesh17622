2c013f2910a2a385fc19fb1ac5d37c136b1de810
rupesh17622 <f20250684@pilani.bits-pilani.ac.in>
2026-09-19 18:20:33 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -1,8 +1,6 @@
 // lut.v
 // A small parameterized ROM (lookup table): DEPTH words, each WIDTH bits
 // wide. dout continuously reflects mem[sel].
-//
-// YOU complete the two TODOs below. Everything else is given.
 
 module lut #(
   parameter WIDTH = 8,
@@ -12,17 +10,21 @@ module lut #(
   output reg [WIDTH-1:0]         dout
 );
 
+  // Storage array declaration
   reg [WIDTH-1:0] mem [0:DEPTH-1];
 
   integer i;
 
-  // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
-  // Use an initial block with a for loop -- this is the only place a ROM's
-  // contents should be set up. (See the lab manual for why.)
-  
+  // Initialize ROM contents at simulation start (i * i lookup table)
+  initial begin
+    for (i = 0; i < DEPTH; i = i + 1) begin
+      mem[i] = i * i;
+    end
+  end
 
-  // TODO: make dout continuously reflect mem[sel]. This is a combinational
-  // read -- pick the right procedural block and sensitivity list.
-
+  // Combinational read: dout continuously updates when sel or mem changes
+  always @(*) begin
+    dout = mem[sel];
+  end
 
 endmodule
--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,27 +1,39 @@
 // tb.v
-// Starter testbench template -- YOU complete this file.
+// Testbench for LUT
 
 module tb;
 
-  // TODO: declare the inputs and outputs
+  // Inputs and outputs
+  reg [1:0] sel;
+  wire [7:0] dout;
 
-  // TODO: instantiate DUT here
+  // Instantiate LUT
+  lut U1 (
+    .sel(sel),
+    .dout(dout)
+  );
 
-  // Waveform dump configuration (DO NOT CHANGE)
+  // Waveform dump configuration
   string vcd_file;
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+      $dumpvars(0, U1);
     end
   end
 
+  // Apply stimulus
   initial begin
-    // TODO: apply different input combinations
-
+    sel = 2'b00;
+    #5 sel = 2'b01;
+    #5 sel = 2'b10;
+    #5 sel = 2'b11;
+    #5 $finish;
   end
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+  // Display outputs in console
+  initial begin
+    $monitor($time, " sel=%b | dout=%d", sel, dout);
+  end
 
 endmodule
